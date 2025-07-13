7c9d01acfa45aa170a94daa417444186eef40a97
inhumane-ops <dogepipia41575@gmail.com>
2025-07-13 16:15:08 +0400

    განახლება

---
+++ day 1/homework/homework 1/lesson .py
@@ -0,0 +1,15 @@
+name = "temo"
+
+print(name)
+
+name = "luka"
+
+print(name)
+
+name = "nona"
+
+print(name)
+
+name = "anzori"
+
+print(name)
---
+++ day 1/homework/homework 1/lesson 1.py
@@ -0,0 +1,15 @@
+name = "dachi" 
+
+print(name)
+
+age = 15
+
+print(age)
+
+country = "georgia"
+
+print(country)
+
+price = 4.99
+
+print(price)
