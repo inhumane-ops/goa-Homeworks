0c7951cafd086d48573bde8a480b846245950733
inhumane-ops <dogepipia41575@gmail.com>
2025-07-25 20:37:10 +0400

    განახლება

---
+++ day 3/homework/lesson.py
@@ -0,0 +1,61 @@
+number1 = 59
+number2 = 60
+number3 = 11
+print(number1+number2+number3)
+
+x = 2340
+y = 7660
+print(x+y)
+
+lenght = 20
+width = 10
+
+პერიმეტრი = 2 * (lenght+width)
+
+print(პერიმეტრი)
+
+# მიმატება
+print(3 + 2)
+
+print(10 + 5)
+
+print(100 + 200)
+
+print(1 + 9)
+
+print(15 + 25)
+
+
+# გამოკლება
+print(10 - 3)
+
+print(20 - 5)
+
+print(100 - 70)
+print(50 - 25)
+print(9 - 4)
+
+# გამრავლება
+print(2 * 3)
+print(5 * 5)
+print(10 * 4)
+print(7 * 6)
+
+
+
+print(12 * 11)
+
+
+# გაყოფა
+print(10 / 2)
+print(9 / 3)
+print(100 / 4)
+print(20 / 5)
+print(50 / 10)
+
+# პროცენტები
+print(10 % 3)
+print(9 % 2)
+print(15 % 4)
+print(20 % 6)
+print(7 % 5)
