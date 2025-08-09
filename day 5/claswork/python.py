b1f2b00674750a5c351389deac497e73e85cb8bd
inhumane-ops <dogepipia41575@gmail.com>
2025-08-09 11:47:19 +0400

    განახლება

---
+++ day 5/claswork/python.py
@@ -0,0 +1,41 @@
+# Debugging ნიშნავს შეცდომების პოვნასა და გასწორებას პროგრამაში.
+
+# Case Sensitive ნიშნავს იმას რომ პროგრამული ენა განასხვავებს პატარა და დიდ ასოებს.Python არის Case Sensitive ენა, ანუ ის განასხვავებს პატარა და დიდ სიმბოლოებს.
+
+user_name = "მერაბი ქაბაბიანი"
+user_age = 21
+favorite_color = "სტაფილოს ფერი (იასნა)"
+number_of_pets = "8-9 (ზუსტად არ ვიცი ზოგი მოკვდა :C )"
+birth_year = "1967 ('როდესაც მუსტანგ ფორდი შეიქმნა ჩემი ფავორიტი')"
+
+print(user_name)
+print(user_age)
+print(favorite_color)
+print(number_of_pets)
+print(birth_year)
+
+# 1num = 10         # შეცდომა: ცვალდეის სახელი ნომრით არ იწერება თორე ეგრევე ციხეში
+# my-name = 20      # შეცდომა: აქ ქვედატირე აკლია ტირე არ შეიძლება
+#try! = 200 # შეცდომა: სიმბოლო "!" არ შეიძლება ცვლადის სახელში.
+# print = გუდ დაი  # შეცდომა: გუდ დაი უნდა იყოს ფრჩხილებში
+# total$amount = 50 # შეცდომა: ცვლადის სახელში სპეციალური სიმბოლოა ფულის სიმბოლო და ეს არ შეიძლება რადგან ქვედატირე უნდა იყოს
+
+num1 = 15
+num2 = 4
+sum_result = num1 + num2
+print( sum_result)
+
+difference = num1 - num2
+print( difference)
+
+uhh_idk = num1 * num2
+print( uhh_idk)
+
+chigga = num1 / num2
+print( chigga)
+
+იზვინიწე = num1 // num2
+print( იზვინიწე)
+
+ჩემი_პროცენტები = num1 % num2
+print(ჩემი_პროცენტები )
---
+++ day 5/homework/python.py
@@ -0,0 +1,65 @@
+# კომენტარი  არის კოდი რომელსაც Python ანუ კომპიუტერი არ ასრულებს იგი გამოიყენება კოდის ახსნისთვის ან შეამოწმებელი ჩანიშვენბის დასამატებლად
+# კომენტარის გამოყენებეა ასევე იმიტომა კარგი პრაქტიკა რომ ჩვენ ამით სხვა პროგრამისტებს ან თვითონ ჩვენ თავს გავაგებინებთ თუ რა ხდება ამ კოდში და კოდის დანიშნულება რო გავიგოტ კიდევაც.
+
+my_name = "Dachi"
+my_surname = "Pipia"
+my_age = 15
+my_city = "Tbilisi"
+my_country = "Georgia"
+my_favorite_food = "xinkali"
+my_favorite_color = "orange"
+my_favorite_hobby = "Programming"
+my_pet_name = "lordi"
+my_university = "TSU"
+
+print(my_name)
+print(my_surname)
+print(my_age)
+print(my_city)
+print(my_country)
+print(my_favorite_food)
+print(my_favorite_color)
+print(my_favorite_hobby)
+print(my_pet_name)
+print(my_university)
+
+
+name1 = "Luka"
+age_num = 56
+my_fav_mov = "Figther in the wind იმენა ვოვ რა უნდა უყუროთ absolute cinema"
+my_friends_name = "Nika"
+
+print("My name is " + name1)
+print("My age is " + str(age_num))
+print("My Favorite Movie is " + my_fav_mov)
+print("My Friend's name is " + my_friends_name)
+
+
+first_name = "Luka"
+last_name = "merabi!"
+full_name = first_name + " " + last_name
+print(full_name)
+
+
+
+city = "Tbilisi"
+country = "Georgia"
+location = city + " " + country
+print(location)
+
+food = "xinkali"
+drink = "Lemonade"
+menu = food + " " + drink
+print(menu)
+
+brand = "Apple"
+product = "iPhone"
+device = brand + " " + product
+print(device)
+
+greeting = "Hello"
+name = "Ana"
+message = greeting + " " + name
+print(message)
+
+# snake_case არის სტილი  სადაც ცვლადის სახელში ყველა ასო პატარაა  და სიტყვები ერთმანეთისგან გამოყოფილია ქვედა ტირეთი როგორცა ზდაროვა_სვერჩიკ  შე_კარტოფილის_გამყიდველო ნასკი_ნასკდოლარე და ასე შემდეგ
